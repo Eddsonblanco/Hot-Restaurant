7d5d2e19f398d20b1b1ecd77fa66eadd0c2075d0
unknown <diesol42@gmail.com>
2019-06-06 20:00:11 -0700

    made pathways

--- server.js
+++ server.js
@@ -10,3 +10,18 @@ app.use(express.json());
 app.listen(PORT, function() {
     console.log("App listening on PORT " + PORT);
 })
+
+const tables = [];
+const waiting = [];
+
+app.get("/", function(req, res) {
+    res.sendFile(path.join(__dirname, "index.html"));
+})
+
+app.get("/reservation", function(req, res) {
+    res.sendFile(path.join(__dirname, "reservation.html"));
+})
+
+app.get("/view", function(req, res) {
+    res.sendFile(path.join(__dirname, "view.html"));
+})
